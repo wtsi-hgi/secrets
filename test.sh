44e12ee0356b67fc3ec932e4d5cf4f8a756e0ae8
Christopher Harrison <ch12@sanger.ac.uk>
2017-07-28 10:33:42 +0100

    Corrected key checking function for GnuPG pre-2.1 and added test

--- test.sh
+++ test.sh
@@ -4,6 +4,24 @@
 # Author: Christopher Harrison <ch12@sanger.ac.uk>
 # Copyright (c) 2017 Genome Research Ltd.
 
+TEST_KEY_ID=""
+
+_gen_key() {
+  # Create valid encryption and signing key
+  gpg2 --batch --gen-key <(cat <<-EOF
+	Key-Type: default
+	Key-Usage: sign
+	Subkey-Type: default
+	Subkey-Usage: encrypt
+	Name-Real: Testy McTestface
+	Name-Email: testy@mctestface.com
+	Expire-Date: 0
+	Passphrase: abc123
+	%commit
+	EOF
+  ) 2>&1 | grep -Eo "key [A-F0-9]+" | cut -d" " -f2
+}
+
 test_stderr() {
   assertEquals "foo" "$(stderr "foo" 2>&1)"
 }
@@ -21,6 +39,16 @@ test_has_dependencies() {
     assertFalse "has_dependencies \"${dep}i\""
   done
   assertFalse "has_dependencies ${bad_deps[*]}"
+}
+
+test_has_valid_secret_keys() {
+  # This should fail before any secret keys have been defined
+  assertFalse "has_valid_secret_keys"
+
+  TEST_KEY_ID="$(_gen_key)"
+
+  # Now we should be good to go
+  assertTrue "has_valid_secret_keys"
 }
 
 # Run tests
